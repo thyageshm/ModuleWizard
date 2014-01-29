9f1cec12420566d810ea3bc3a0ce0149480c17c7
Ashray Jain <ashrayj11@gmail.com>
2014-01-29 18:02:37 +0800

    Added timetable endpoint

--- app/scripts/timetable_data_endpoint.py
+++ app/scripts/timetable_data_endpoint.py
@@ -1,17 +1,17 @@
 from base_handler import *
 import json
 
-
 data = json.load(open("app/data/modInfo.json"), encoding='latin1')
 
 class TimeTableDataEndpoint(Handler):
     def get(self):
         module = self.request.get('module').upper()
-        sem = self.request.get('sem')
+        sem = "Sem"+self.request.get('sem')
         self.response.headers['Content-Type'] = "application/json"
         self.response.headers['Cache-Control'] = "max-age=30"
-        if module in data:
-            self.response.out.write(json.dumps(data[modName]))
-            return
-        self.response.out.write(json.dumps({}))
+
+        if module in data and sem in data[module]["Timetable"] and isinstance(data[module]["Timetable"][sem], list):
+            self.response.out.write(json.dumps(data[module]["Timetable"][sem]))
+        else:
+            self.response.out.write(json.dumps({}))
         return
--- index.py
+++ index.py
@@ -7,8 +7,10 @@ from base_handler import *
 from ivle import *
 from step_one import *
 from step_two import *
+from timetable_data_endpoint import *
 
 # Handler Routing
 app = webapp2.WSGIApplication([('/', StepOneHandler),
     ('/ivle', IVLEHandler),
-    ('/preallocation', StepTwoHandler)], debug=True)
+    ('/preallocation', StepTwoHandler),
+    ('/timetableData', TimeTableDataEndpoint)], debug=True)
